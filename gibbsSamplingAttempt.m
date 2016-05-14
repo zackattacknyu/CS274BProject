04118ce80eb27c24659f8d01624e62e6dc8ed346
zackattacknyu <zackattacknyu@gmail.com>
2016-05-14 05:01:07 -0700

    made progress on sampling

--- gibbsSamplingAttempt.m
+++ gibbsSamplingAttempt.m
@@ -1,12 +1,14 @@
 
-curModelNum = 6;
+curModelNum = 3;
 curModel = models_test{curModelNum};
 curModelPairs = curModel.pairs;
 
 cliqueIndAsI = curModel.N1;
 cliqueIndAsJ = curModel.N2;
 
-nodeNum = 1250;
+%nodeNum = 12500;
+nodeNum = 271001; %greatest class 3 prob
+%nodeNum = 77123; %greatest class 2 prob
 
 curIcliques = cliqueIndAsI(nodeNum,:);
 curIcliques = curIcliques(curIcliques>0);
@@ -14,17 +16,17 @@ curJcliques = cliqueIndAsJ(nodeNum,:);
 curJcliques = curJcliques(curJcliques>0);
 
 currentBi = ones(1,3);
-jValues = [2 3];
+jValues = [1 2];
 iValues = [3 3];
 
 %Backward Neighbors
 for j = 1:length(curJcliques)
        
-    curClique = curJcliques(i);
+    curClique = curJcliques(j);
     %curPair2 = curModelPairs(curJcliques(j),:)
     %curNeigh = curPair(1);
     
-    curI = iValues(i);
+    curI = iValues(j);
     currentBij = reshape(b_ij(:,curClique),3,3);
     currentBijGivenI = currentBij./repmat(sum(currentBij,2),1,3);
     currentBi = currentBi.*currentBijGivenI(curI,:);
@@ -35,11 +37,17 @@ currentBi = currentBi';
 for i = 1:length(curIcliques)
     curClique = curIcliques(i);
     %curPair = curModelPairs(curIcliques(i),:)
-    curNeigh = curPair(2);
+    %curNeigh = curPair(2);
     
     curJ = jValues(i);
     currentBij = reshape(b_ij(:,curClique),3,3);
     currentBijGivenJ = currentBij./repmat(sum(currentBij,1),3,1);
     currentBi = currentBi.*currentBijGivenJ(:,curJ);
 end
+
+newProbXi = currentBi./sum(currentBi);
+
+%sample from the conditional distribution
+randSample = rand;
+randLabel = find(randSample<cumsum(newProbXi),1,'first')
 
