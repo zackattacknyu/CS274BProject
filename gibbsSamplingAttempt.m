129b6c6c64eeb7ad02b884d9c6afd21dfa4eecef
zackattacknyu <zackattacknyu@gmail.com>
2016-05-14 18:31:47 -0700

    started

--- gibbsSamplingAttempt.m
+++ gibbsSamplingAttempt.m
@@ -5,9 +5,17 @@ curModelPairs = curModel.pairs;
 
 cliqueIndAsI = curModel.N1;
 cliqueIndAsJ = curModel.N2;
+%%
+
+sizr = 500; sizc = 750;
+initialY = ceil(rand(sizr,sizc)*3);
+imagesc(initialY);colorbar;
+
+%%
 
 %nodeNum = 12500;
-nodeNum = 271001; %greatest class 3 prob
+%nodeNum = 271001; %greatest class 3 prob
+nodeNum = 271041;
 %nodeNum = 77123; %greatest class 2 prob
 
 curIcliques = cliqueIndAsI(nodeNum,:);
@@ -19,12 +27,12 @@ currentBi = ones(1,3);
 jValues = [1 2];
 iValues = [3 3];
 
-%Backward Neighbors
+%Backward Neighbors, use current values
 for j = 1:length(curJcliques)
        
     curClique = curJcliques(j);
-    %curPair2 = curModelPairs(curJcliques(j),:)
-    %curNeigh = curPair(1);
+    curPair2 = curModelPairs(curJcliques(j),:);
+    curNeighIndex = curPair2(1)
     
     curI = iValues(j);
     currentBij = reshape(b_ij(:,curClique),3,3);
@@ -33,11 +41,11 @@ for j = 1:length(curJcliques)
 end
 currentBi = currentBi';
 
-%Forward Neighbors
+%Forward Neighbors, use previous values
 for i = 1:length(curIcliques)
     curClique = curIcliques(i);
-    %curPair = curModelPairs(curIcliques(i),:)
-    %curNeigh = curPair(2);
+    curPair = curModelPairs(curIcliques(i),:);
+    curNeighIndex = curPair(2)
     
     curJ = jValues(i);
     currentBij = reshape(b_ij(:,curClique),3,3);
@@ -49,5 +57,5 @@ newProbXi = currentBi./sum(currentBi);
 
 %sample from the conditional distribution
 randSample = rand;
-randLabel = find(randSample<cumsum(newProbXi),1,'first')
+randLabel = find(randSample<cumsum(newProbXi),1,'first');
 
