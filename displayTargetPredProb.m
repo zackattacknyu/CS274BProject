846da272cb89b626ce9265fb6364f8b9b89582db
zackattacknyu <zackattacknyu@gmail.com>
2016-07-02 20:00:32 -0700

    res

--- displayTargetPredProb.m
+++ displayTargetPredProb.m
@@ -14,14 +14,14 @@ labelsDisp(curTargetLabels>=3)=2;
 
 
 figure
-subplot(1,3,1)
+subplot(1,2,1)
 imagesc(labelsDisp);
 title('Target Precipitation Image');
 %caxis([-1 20]) 
 drwvect([-130 25 -100 45],[500 750],'us_states_outl_ug.tmp','k')
 %colorbar('vertical')
 
-subplot(1,3,2);
+subplot(1,2,2);
 imagesc(curProb3wholeMap); 
 title('Probability of Precipitation');
 colorbar; 
@@ -29,12 +29,7 @@ colormap jet;
 drwvect([-130 25 -100 45],[500 750],'us_states_outl_ug.tmp','k')
 colorbar('vertical')
 
-subplot(1,3,3)
-imagesc(x_predDisp);
-title('Predicted Precipitation Image');
-%caxis([-1 20]) 
-drwvect([-130 25 -100 45],[500 750],'us_states_outl_ug.tmp','k')
-%colorbar('vertical')
+
 
 drawnow
 end
