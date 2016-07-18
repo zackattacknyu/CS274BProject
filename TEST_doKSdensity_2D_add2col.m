8432d4b115b1caf905fab668e206b95ba5dbd2ac
zackattacknyu <zackattacknyu@gmail.com>
2016-07-18 03:18:11 -0700

    dfs

---
+++ TEST_doKSdensity_2D_add2col.m
@@ -0,0 +1,132 @@
+sizr = 500;
+sizc = 750;
+rho = 0.5;
+nvals = 2;
+
+yFiles11 = dir('projectData/ytarget1109*');
+xFiles11 = dir('projectData/xdata1109*');
+ccsFiles11 = dir('projectData/ccspred1109*');
+xOneFiles11 = dir('projectData/xone1109*');
+
+yFiles12 = dir('projectData/ytarget1209*');
+xFiles12 = dir('projectData/xdata1209*');
+ccsFiles12 = dir('projectData/ccspred1209*');
+xOneFiles12 = dir('projectData/xone1209*');
+segFiles12 = dir('projectData/seg1209*');
+
+loss_spec = 'trunc_cl_trwpll_5';
+crf_type  = 'linear_linear';
+options.print_times = 0; % since this is so slow, print stuff to screen
+options.gradual     = 1; % use gradual fitting
+options.maxiter     = 1000;
+options.rho         = rho;
+options.reg         = 1e-4;
+options.opt_display = 0;
+
+
+%load('domkeCRFrun_3edgeFeats_cliqueLoss_new3','p','validationInds');
+load('ROCvars_sep2012_3edgeFeats_cliqueLoss_testInds_new3','trialInds2');
+
+%trialInds2 = trialInds2(1:4);
+
+[feats_test,efeats_test,labels_test,models_test,precipImages_test,ccsLabels,ccsYvalues] = ...
+    obtainDataFromFiles3_add2col(trialInds2,...
+    xFiles12,yFiles12,ccsFiles12,xOneFiles12,segFiles12);
+
+XdataTest = [];
+YdataTest = [];
+
+for i = 1:length(feats_test)
+    i
+    XdataTest = [XdataTest;feats_test{i}];
+    YdataTest = [YdataTest;floor(labels_test{i}(:))];
+end
+
+testPixels2 = find(YdataTest>1);
+
+f3arrays = cell(1,13);
+f2arrays = cell(1,13);
+xi3arrays = cell(1,13);
+xi2arrays = cell(1,13);
+
+YY2 = YdataTest(testPixels2)-2;
+precipInds = find(YY2>0.9);
+noPrecipInds = find(YY2<0.1);
+
+XXtemp = XdataTest(testPixels2,1);
+tempValsPrecip = XXtemp(precipInds);
+tempValsNoPrecip = XXtemp(noPrecipInds);
+
+numFeats = 17;
+numPts = 32;
+allTemps = [tempValsPrecip; tempValsNoPrecip];
+minTemp = min(allTemps); maxTemp = max(allTemps);
+
+precipDensNorm = cell(1,numFeats);
+noPrecipDensNorm = cell(1,numFeats);
+
+precipDensUnNorm = cell(1,numFeats);
+noPrecipDensUnNorm = cell(1,numFeats);
+
+for feat = 1:numFeats
+    XX2 = XdataTest(testPixels2,feat);
+    featValsPrecip = XX2(precipInds);
+    featValsNoPrecip = XX2(noPrecipInds);
+    
+    allFeats = [featValsPrecip; featValsNoPrecip];
+    minFeat = min(allFeats); maxFeat = max(allFeats);
+    
+    matrixPrecip = [tempValsPrecip featValsPrecip];
+    matrixNoPrecip = [tempValsNoPrecip featValsNoPrecip];
+    
+    [~,dens1] = kde2d(matrixPrecip,numPts,[minTemp,minFeat],[maxTemp,maxFeat]);
+    [~,dens2] = kde2d(matrixNoPrecip,numPts,[minTemp,minFeat],[maxTemp,maxFeat]);
+    
+    precipDensNorm{feat} = dens1./sum(dens1(:));
+    noPrecipDensNorm{feat} = dens2./sum(dens2(:));
+    
+    precipDensUnNorm{feat} = dens1;
+    noPrecipDensUnNorm{feat} = dens2;
+
+end
+%%
+save('KSdensity2dVars_sep2012_new3inds_with2Col.mat','precipDensNorm',...
+    'noPrecipDensNorm','precipDensUnNorm','noPrecipDensUnNorm');
+%%
+load('KSdensity2dVars_sep2012_new3inds_with2Col')
+%%
+for feat = 1:numFeats
+   figure
+   subplot(1,2,1)
+   imagesc(precipDensNorm{feat}); 
+   colormap jet;
+   colorbar;
+   
+   subplot(1,2,2)
+   imagesc(noPrecipDensNorm{feat});
+   colormap jet;
+   colorbar;
+end
+%%
+for feat = [2:13 15:numFeats]
+    displayData = [precipDensUnNorm{feat} noPrecipDensUnNorm{feat}];
+    allDispData = displayData(:);
+    clims = [min(allDispData) max(allDispData)];
+    if(isnan(clims(1)))
+        feat
+       continue; 
+    end
+    
+   figure
+   subplot(2,1,1)
+   imagesc(precipDensUnNorm{feat},clims); 
+   colormap jet;
+   colorbar;
+   axis off
+   
+   subplot(2,1,2)
+   imagesc(noPrecipDensUnNorm{feat},clims);
+   colormap jet;
+   colorbar;
+   axis off
+end
---
+++ TEST_doKSdensity_add2col.m
@@ -0,0 +1,85 @@
+sizr = 500;
+sizc = 750;
+rho = 0.5;
+nvals = 2;
+
+yFiles11 = dir('projectData/ytarget1109*');
+xFiles11 = dir('projectData/xdata1109*');
+ccsFiles11 = dir('projectData/ccspred1109*');
+xOneFiles11 = dir('projectData/xone1109*');
+
+yFiles12 = dir('projectData/ytarget1209*');
+xFiles12 = dir('projectData/xdata1209*');
+ccsFiles12 = dir('projectData/ccspred1209*');
+xOneFiles12 = dir('projectData/xone1209*');
+segFiles12 = dir('projectData/seg1209*');
+
+
+loss_spec = 'trunc_cl_trwpll_5';
+crf_type  = 'linear_linear';
+options.print_times = 0; % since this is so slow, print stuff to screen
+options.gradual     = 1; % use gradual fitting
+options.maxiter     = 1000;
+options.rho         = rho;
+options.reg         = 1e-4;
+options.opt_display = 0;
+
+
+%load('domkeCRFrun_3edgeFeats_cliqueLoss_new3','p','validationInds');
+load('ROCvars_sep2012_3edgeFeats_cliqueLoss_testInds_new3','trialInds2');
+%trialInds2 = trialInds2(1:4);
+
+[feats_test,efeats_test,labels_test,models_test,precipImages_test,ccsLabels,ccsYvalues] = ...
+    obtainDataFromFiles3_add2col(trialInds2,...
+    xFiles12,yFiles12,ccsFiles12,xOneFiles12,segFiles12);
+
+XdataTest = [];
+YdataTest = [];
+%%
+for i = 1:length(feats_test)
+    i
+    XdataTest = [XdataTest;feats_test{i}];
+    YdataTest = [YdataTest;floor(labels_test{i}(:))];
+end
+
+testPixels2 = find(YdataTest>1);
+
+numFeats = 17;
+
+f3arrays = cell(1,numFeats);
+f2arrays = cell(1,numFeats);
+xi3arrays = cell(1,numFeats);
+xi2arrays = cell(1,numFeats);
+
+for feat = 1:numFeats
+    XX2 = XdataTest(testPixels2,feat);
+    YY2 = YdataTest(testPixels2)-2;
+
+    precipInds = find(YY2>0.9);
+    noPrecipInds = find(YY2<0.1);
+
+    tempValsPrecip = XX2(precipInds);
+    tempValsNoPrecip = XX2(noPrecipInds);
+
+    [f3,xi3] = ksdensity(tempValsPrecip);
+    [f2,xi2] = ksdensity(tempValsNoPrecip);
+
+    f3arrays{feat} = f3;
+    f2arrays{feat} = f2;
+    xi3arrays{feat} = xi3;
+    xi2arrays{feat} = xi2;
+
+end
+
+save('KSdensityVars_sep2012_new3inds_with2Col.mat','f3arrays','f2arrays','xi3arrays','xi2arrays');
+%%
+load('KSdensityVars_sep2012_new3inds_with2Col')
+
+for feat=1:numFeats
+    figure
+    hold on
+    plot(xi2arrays{feat},f2arrays{feat},'g-');
+    plot(xi3arrays{feat},f3arrays{feat},'r-');
+    legend('No Precip Inds','Precip Inds');
+    hold off 
+end
